a76a8ce936ae4a05f94fae9914d0c51249dde470
catharsis29 <92992346+catharsis29@users.noreply.github.com>
2022-01-10 17:49:50 +0800

    add styles

--- js/main.js
+++ js/main.js
@@ -9,28 +9,28 @@ function closeModal() {
 
 //   PopUp
 
-let popupBg = document.querySelector('.popup-bg');
-let popup = document.querySelector('.popup');
-let openPopupButtons = document.querySelectorAll('.open-popup');
-let closePopupButton = document.querySelector('.close-popup');
+let popupBg = document.querySelector('.popup-bg'); // Фон попап окна
+let popup = document.querySelector('.popup'); // Само окно
+let openPopupButtons = document.querySelectorAll('.open-popup'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
 
-openPopupButtons.forEach((button) => {
-    button.addEventListener('click', (e) => { 
-        e.preventDefault();
-        popupBg.classList.add('active');
-        popup.classList.add('active');
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+        popup.classList.add('active'); // И для самого окна
     })
 });
 
-closePopupButton.addEventListener('click',() => {
-    popupBg.classList.remove('active');
-    popup.classList.remove('active');
+closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
+    popupBg.classList.remove('active'); // Убираем активный класс с фона
+    popup.classList.remove('active'); // И с окна
 });
 
-document.addEventListener('click', (e) => {
-    if(e.target === popupBg) {
-        popupBg.classList.remove('active');
-        popup.classList.remove('active');
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+    if(e.target === popupBg) { // Если цель клика - фон, то:
+        popupBg.classList.remove('active'); // Убираем активный класс с фона
+        popup.classList.remove('active'); // И с окна
 }
 });
 // Smooth Scroll
@@ -49,3 +49,15 @@ function clickHandler(e) {
         behavior: "smooth"
     });
 }
+
+// Mask-tel
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const inputElement = document.querySelector('.form-tel') // ищем наш input
+    const maskOptions = { // создаем объект параметров
+      mask: '+{7} (000) 000 - 00 - 00' // задаем единственный параметр mask
+    }
+    IMask(inputElement, maskOptions) // запускаем плагин с переданными параметрами
+
+})
